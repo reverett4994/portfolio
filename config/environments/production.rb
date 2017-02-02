534aa8f4d67ee2a654a002169b8a3b6f9e596944
Ryan Everett <reverett4994@gmail.com>
2017-02-02 03:06:47 -0500

    added mailer into production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,18 @@ s3_region: ENV.fetch('AWS_REGION'),
 }
 #FOUND ON DEVISE PAGE TO USE WITH HEROKU
 config.assets.initialize_on_precompile = false
+
+config.action_mailer.default_url_options = { host: 'YourPortfolio.com'}
+config.mailer_sender = 'YourPortfolio'
+
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
